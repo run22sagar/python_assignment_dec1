b6903f34401dc4980fb773c86c058d71f793dd73
run22sagar <run2sagar@gmail.com>
2017-12-02 22:07:58 +0545

    A function which accepts  friend's name as a argument and prints a Happy Birthday song for him/her.

---
+++ birthday_wish.py
@@ -0,0 +1,5 @@
+def birthday(name):
+    print ("{0} {1} {2} {3}!\n{0} {1} {2} {3}!\n{0} {1} {4} {5}\n{0} {1} {2} {3}!!".format("Happy","Birthday","To","You","Dear",name))
+
+F_name = input("Enter Friend's Name: ")
+birthday(F_name)
