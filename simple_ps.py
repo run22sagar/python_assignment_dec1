e1a512f16a52e24782b17b760095bcf60571a236
run22sagar <run2sagar@gmail.com>
2017-12-03 13:22:20 +0545

    A simple python script learned till now

---
+++ simple_ps.py
@@ -0,0 +1,33 @@
+#to define global variable
+PI = 3.14
+
+#to find the sum
+a=5
+b=7
+sum=a+b
+print(sum)
+
+#to find the square
+c=5
+square=c**2
+print(square)
+
+#to find division with floating and single digit
+x=6
+y=5
+div1=x/y
+div2=x//2
+print(div1)
+print(div2)
+
+#to declare string
+print("Nepal is our country")
+print("It\'s so fun")
+
+#to concanicate string
+str1="Jay "
+str2="Nepal"
+print(str1+str2)
+
+#to display text in multiple times
+print(str1 * 10)
